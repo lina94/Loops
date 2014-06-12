5e3d21e855a6020c23b6accd40fd7ffdfeadc79c
lina94 <katalina_dimitrova@yahoo.com>
2014-06-12 22:21:39 +0300

    try
    try

---
+++ 01.PrintingNumbersToN/PrintingNumbersToN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// 1. Write a program that prints all the numbers from 1 to N.
+
+namespace PrintingNumbersToN
+{
+    class PrintingNumbersToN
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int N;
+
+            // checking if the input is correct
+            do
+            {
+                Console.WriteLine("Type a number N: ");
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out N) || (N <= 0)); 
+
+            // printing the numbers
+            for (int counter = 1; counter <= N; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
---
+++ 02.PrintingDivisibleNumbers/PrintingDivisibleNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 2. Write a program that prints all the numbers from 1 to N, that are not divisible by 3 and 7 at the same time.
+
+namespace _02.PrintingDivisibleNumbers
+{
+    class PrintingDivisibleNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type a number N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int number = 1; number <= N; number++)
+            {
+                if (number % (7*3) != 0) // checking if the number is not divided by 7 and 3
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+        }
+    }
+}
